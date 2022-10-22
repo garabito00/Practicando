e783f444eb141ce022e8ae28d22fc7064deab08e
garabito00 <91238183+garabito00@users.noreply.github.com>
2022-10-22 13:18:28 -0400

    Add files via upload

---
+++ WebApplication1/Areas/Paystub/Controllers/PosicionController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Models;
+using Models.ViewModel;
+using Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace WebApplication1.Areas.Paystub.Controllers
+{
+    [Area("paystub")]
+    public class PosicionController : Controller
+    {
+        private IUnitOfWork _unitOfWork;
+
+        public PosicionController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        #region Index
+        public IActionResult Index()
+        {
+            PositionVM model = new PositionVM
+            {
+                Posiciones = _unitOfWork.Posicion.PosicionesConProyecto()
+            };
+
+            return View(model);
+        }
+
+        #endregion
+
+
+        #region Create
+        public IActionResult Create()
+        {
+            PositionVM model = new PositionVM()
+            {
+                Posicion = new Posicion(),
+                ListaProyectos = new System.Web.Mvc.SelectList(_unitOfWork.Proyecto.GetAll().ToList(), "proyecto_id", "compania")
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(PositionVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Posicion.Add(model.Posicion);
+                _unitOfWork.Save();
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+        #endregion
+
+
+    }
+}
---
+++ WebApplication1/Areas/Paystub/Controllers/ProyectoController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models;
+using Models.ViewModel;
+using Repository.Context;
+using Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Areas.Paystub.Controllers
+{
+    [Area("Paystub")]
+    public class ProyectoController : Controller
+    {
+        private IUnitOfWork _unitOfWork;
+
+        public ProyectoController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        #region Index
+        public IActionResult Index()
+        {
+            ProyectoVM model = new ProyectoVM
+            {
+                ProyectosLista = _unitOfWork.Proyecto.GetAll()
+            }; 
+
+            return View(model);
+        }
+        #endregion
+
+        #region Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ProyectoVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Proyecto.Add(model.Proyecto);
+                _unitOfWork.Save();
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+        #endregion
+
+        #region Edit
+        public IActionResult Edit(int? id)
+        {
+            if (id == 0 || id == null)
+            {
+                return NotFound();
+            }
+
+            ProyectoVM model = new ProyectoVM();
+            model.Proyecto = _unitOfWork.Proyecto.FindOne(p => p.Proyecto_Id == id);
+
+            if(model.Proyecto == null)
+            {
+                return NotFound();
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(ProyectoVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Proyecto.Update(model.Proyecto);
+                _unitOfWork.Save();
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+        #endregion
+
+        #region Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == 0 || id == null)
+            {
+                return NotFound();
+            }
+
+            ProyectoVM model = new ProyectoVM();
+            model.Proyecto = _unitOfWork.Proyecto.FindOne(p => p.Proyecto_Id == id);
+
+            if (model.Proyecto == null)
+            {
+                return NotFound();
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(ProyectoVM model)
+        {
+            _unitOfWork.Proyecto.Delete(model.Proyecto);
+            _unitOfWork.Save();
+            return RedirectToAction("Index");
+        }
+        #endregion
+    }
+}
