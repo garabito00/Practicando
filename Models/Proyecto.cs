cf54b151d8da41697fd0a1fef5bed4ae7ecb3af9
garabito00 <91238183+garabito00@users.noreply.github.com>
2022-10-22 13:16:30 -0400

    nuevos archivos

--- Models/Proyecto.cs
+++ Models/Proyecto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Models
@@ -7,6 +8,8 @@ namespace Models
     public class Proyecto
     {
         public int Proyecto_Id { get; set; }
+        [Required]
+        [Display(Name = "Compañia")]
         public string Compania { get; set; }
         public IEnumerable<Posicion> Posiciones { get; set; }
     }
---
+++ Models/ViewModel/ProyectoVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.ViewModel
+{
+    public class ProyectoVM
+    {
+        public IEnumerable<Proyecto> ProyectosLista { get; set; }
+        public Proyecto Proyecto { get; set; }
+
+    }
+}
